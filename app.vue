60eb4428cf25eadda3be07f15545edda55e59c20
salmandma <salmandwimaulanaakbar96@gmail.com>
2023-12-28 15:54:24 +0700

    initial commit

---
+++ app.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const { status, signIn, signOut } = useAuth();
+
+const loggedIn = computed(() => status.value === 'authenticated');
+
+async function handleSignIn() {
+ const githubProvider = 'github';
+ await signIn(githubProvider);
+}
+
+async function handleSignOut() {
+ await signOut();
+}
+</script>
+
+<template>
+ <div>
+  <button v-if="loggedIn" @click="handleSignOut">Sign Out</button>
+  <button v-else @click="handleSignIn">Sign In</button>
+ </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+export default defineNuxtConfig({
+ devtools: { enabled: true },
+ modules: ['@sidebase/nuxt-auth'],
+ runtimeConfig: {
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+ },
+});
